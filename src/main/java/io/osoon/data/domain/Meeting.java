0380a4cf4189d47d91fd77751dfb6e1314f3806e
Keesun Baik <whiteship2000@gmail.com>
2017-10-23 22:53:30 -0700

    Fix typo

--- src/main/java/io/osoon/data/domain/Meeting.java
+++ src/main/java/io/osoon/data/domain/Meeting.java
@@ -24,7 +24,7 @@ public class Meeting {
 	String titleImage;
 	int maxAttendees;
 
-	@Relationship(type = "MEET_ON")
+	@Relationship(type = "MEET_AT")
 	MeetingLocation location;
 
 	LocalDateTime meetAt;
