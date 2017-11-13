7011559e659c765ec6e4c36b71f74afe14ab268e
Keesun Baik <whiteship@epril.com>
2017-11-12 22:10:29 -0800

    Add test and doc for POST /api/topic/create

--- src/test/java/io/osoon/web/api/TopicControllerTest.java
+++ src/test/java/io/osoon/web/api/TopicControllerTest.java
@@ -1,5 +1,7 @@
 package io.osoon.web.api;
 
+import io.osoon.data.domain.Topic;
+import io.osoon.data.repository.TopicRepository;
 import io.osoon.web.dto.TopicDto;
 import org.hamcrest.Matchers;
 import org.junit.Test;
@@ -10,6 +12,7 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author whiteship
@@ -23,8 +26,8 @@ public class TopicControllerTest extends ControllerTest {
         TopicDto topicDto = TopicDto.of(topic);
 
         MockHttpServletRequestBuilder request = post("/api/topic/create")
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .content(objectMapper.writeValueAsString(topicDto));
+            .contentType(MediaType.APPLICATION_JSON_UTF8)
+            .content(objectMapper.writeValueAsString(topicDto));
 
         // When & Then
         mvc.perform(request)
@@ -32,6 +35,26 @@ public class TopicControllerTest extends ControllerTest {
             .andDo(document("create-topic"))
             .andExpect(jsonPath("$.id").isNotEmpty())
             .andExpect(jsonPath("$.name", Matchers.is(topic)))
+        ;
+    }
+
+    @Test
+    public void createDuplicatedTopic() throws Exception {
+        // Given
+        Topic spring = new Topic("spring");
+        topicRepository.save(spring);
+
+        TopicDto topicDto = TopicDto.of(spring.getName());
+
+        MockHttpServletRequestBuilder request = post("/api/topic/create")
+            .contentType(MediaType.APPLICATION_JSON_UTF8)
+            .content(objectMapper.writeValueAsString(topicDto));
+
+        // When & Then
+        mvc.perform(request)
+            .andDo(print())
+            .andDo(document("create-topic-duplicated"))
+            .andExpect(status().isBadRequest())
         ;
     }
 
