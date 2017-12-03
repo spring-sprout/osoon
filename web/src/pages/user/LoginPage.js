af63cfb4ea4fce16b06b15ea748e855913459d6a
Outsider <outsideris@gmail.com>
2017-12-03 22:32:57 +0900

    design a login page
    close #55

--- web/src/pages/user/LoginPage.js
+++ web/src/pages/user/LoginPage.js
@@ -19,10 +19,12 @@ class LoginPage extends Component {
 
     return (
       <div className="LoginPage">
+        <h1>Welcome to Osoon</h1>
+        <h3>간단하게 가입하고 참여해 보세요!</h3>
         <div className="login-btn facebook-login">
           <form action={SERVER_URL + '/signin/facebook'} method="post">
             <input type="hidden" name="scope" value="public_profile, email" />
-            <input type="image" src={facebookButton} alt="Login with Facebook" />
+            <button type="submit" className="login-btn">페이스북으로 로그인</button>
           </form>
         </div>
       </div>
