779ee120ac9527f01e53709e143537cbf9a10b11
Outsider <outsideris@gmail.com>
2018-04-07 23:41:54 +0900

    fe: style a landing page

--- web/src/pages/FrontPage.js
+++ web/src/pages/FrontPage.js
@@ -14,7 +14,7 @@ class FrontPage extends Component {
             <h1>모임을 만들어 보세요</h1>
             <span>이불 밖도 안전해!</span>
             <span>원하는 모임을 만들어서 오순도순 함께하는 즐거움을 느껴보세요.</span>
-            <Link to="/meetings/create" className="createBtn">지금 만들기</Link>
+            <Link to="/meetings/create" className="createBtn">모임 만들기</Link>
           </div>
         </div>
         <div className="content-body">
@@ -24,120 +24,144 @@ class FrontPage extends Component {
             <Link to="#" className="meeting-item">
               <div className="meeting-image"></div>
               <div className="meeting-item-body">
-                <h2 className="meeting-title">meeting title goes here. meeting title goes here.</h2>
-                <span className="remain">7명 남음</span>
-                <time>2017.12.12</time>
+                <h2 className="meeting-title">Design</h2>
+                <span className="meeting-content">Freelance Design Tricks How To Get Away With Murder in The Workplace</span>
               </div>
-              <div className="progress hurryup" style={{width: 90 + '%'}}></div>
+              <div className="meeting-item-footer">
+                <span className="location">7596 Launine Parkway</span>
+                <time>15 Mar 2018</time>
+              </div>
             </Link>
             <Link to="#" className="meeting-item">
               <div className="meeting-image"></div>
               <div className="meeting-item-body">
-                <h2 className="meeting-title">meeting title goes here. meeting title goes here.</h2>
-                <span className="remain">7명 남음</span>
-                <time>2017.12.12</time>
+                <h2 className="meeting-title">Design</h2>
+                <span className="meeting-content">Freelance Design Tricks How To Get Away With Murder in The Workplace</span>
               </div>
-              <div className="progress" style={{width: 60 + '%'}}></div>
+              <div className="meeting-item-footer">
+                <span className="location">7596 Launine Parkway</span>
+                <time>15 Mar 2018</time>
+              </div>
             </Link>
             <Link to="#" className="meeting-item">
               <div className="meeting-image"></div>
               <div className="meeting-item-body">
-                <h2 className="meeting-title">meeting title goes here. meeting title goes here.</h2>
-                <span className="remain">7명 남음</span>
-                <time>2017.12.12</time>
+                <h2 className="meeting-title">Design</h2>
+                <span className="meeting-content">Freelance Design Tricks How To Get Away With Murder in The Workplace</span>
               </div>
-              <div className="progress" style={{width: 60 + '%'}}></div>
+              <div className="meeting-item-footer">
+                <span className="location">7596 Launine Parkway</span>
+                <time>15 Mar 2018</time>
+              </div>
             </Link>
             <Link to="#" className="meeting-item">
               <div className="meeting-image"></div>
               <div className="meeting-item-body">
-                <h2 className="meeting-title">meeting title goes here. meeting title goes here.</h2>
-                <span className="remain">7명 남음</span>
-                <time>2017.12.12</time>
+                <h2 className="meeting-title">Design</h2>
+                <span className="meeting-content">Freelance Design Tricks How To Get Away With Murder in The Workplace</span>
               </div>
-              <div className="progress" style={{width: 60 + '%'}}></div>
+              <div className="meeting-item-footer">
+                <span className="location">7596 Launine Parkway</span>
+                <time>15 Mar 2018</time>
+              </div>
             </Link>
           </div>
 
           <div className="meeting-list">
-            <Link to="#" className="more">모두 보기</Link>
+            <Link to="#" className="more">모두 보기<img src={rightArrowIcon} alt=""/></Link>
             <h2>신규 모임</h2>
             <Link to="#" className="meeting-item">
               <div className="meeting-image"></div>
               <div className="meeting-item-body">
-                <h2 className="meeting-title">meeting title goes here. meeting title goes here.</h2>
-                <span className="remain">7명 남음</span>
-                <time>2017.12.12</time>
+                <h2 className="meeting-title">Design</h2>
+                <span className="meeting-content">Freelance Design Tricks How To Get Away With Murder in The Workplace</span>
               </div>
-              <div className="progress hurryup" style={{width: 90 + '%'}}></div>
+              <div className="meeting-item-footer">
+                <span className="location">7596 Launine Parkway</span>
+                <time>15 Mar 2018</time>
+              </div>
             </Link>
             <Link to="#" className="meeting-item">
               <div className="meeting-image"></div>
               <div className="meeting-item-body">
-                <h2 className="meeting-title">meeting title goes here. meeting title goes here.</h2>
-                <span className="remain">7명 남음</span>
-                <time>2017.12.12</time>
+                <h2 className="meeting-title">Design</h2>
+                <span className="meeting-content">Freelance Design Tricks How To Get Away With Murder in The Workplace</span>
               </div>
-              <div className="progress" style={{width: 60 + '%'}}></div>
+              <div className="meeting-item-footer">
+                <span className="location">7596 Launine Parkway</span>
+                <time>15 Mar 2018</time>
+              </div>
             </Link>
             <Link to="#" className="meeting-item">
               <div className="meeting-image"></div>
               <div className="meeting-item-body">
-                <h2 className="meeting-title">meeting title goes here. meeting title goes here.</h2>
-                <span className="remain">7명 남음</span>
-                <time>2017.12.12</time>
+                <h2 className="meeting-title">Design</h2>
+                <span className="meeting-content">Freelance Design Tricks How To Get Away With Murder in The Workplace</span>
               </div>
-              <div className="progress" style={{width: 60 + '%'}}></div>
+              <div className="meeting-item-footer">
+                <span className="location">7596 Launine Parkway</span>
+                <time>15 Mar 2018</time>
+              </div>
             </Link>
             <Link to="#" className="meeting-item">
               <div className="meeting-image"></div>
               <div className="meeting-item-body">
-                <h2 className="meeting-title">meeting title goes here. meeting title goes here.</h2>
-                <span className="remain">7명 남음</span>
-                <time>2017.12.12</time>
+                <h2 className="meeting-title">Design</h2>
+                <span className="meeting-content">Freelance Design Tricks How To Get Away With Murder in The Workplace</span>
               </div>
-              <div className="progress" style={{width: 60 + '%'}}></div>
+              <div className="meeting-item-footer">
+                <span className="location">7596 Launine Parkway</span>
+                <time>15 Mar 2018</time>
+              </div>
             </Link>
           </div>
 
           <div className="meeting-list">
-            <Link to="#" className="more">모두 보기</Link>
+            <Link to="#" className="more">모두 보기<img src={rightArrowIcon} alt=""/></Link>
             <h2>곧 마감하는 모임</h2>
             <Link to="#" className="meeting-item">
               <div className="meeting-image"></div>
               <div className="meeting-item-body">
-                <h2 className="meeting-title">meeting title goes here. meeting title goes here.</h2>
-                <span className="remain">7명 남음</span>
-                <time>2017.12.12</time>
+                <h2 className="meeting-title">Design</h2>
+                <span className="meeting-content">Freelance Design Tricks How To Get Away With Murder in The Workplace</span>
               </div>
-              <div className="progress hurryup" style={{width: 90 + '%'}}></div>
+              <div className="meeting-item-footer">
+                <span className="location">7596 Launine Parkway</span>
+                <time>15 Mar 2018</time>
+              </div>
             </Link>
             <Link to="#" className="meeting-item">
               <div className="meeting-image"></div>
               <div className="meeting-item-body">
-                <h2 className="meeting-title">meeting title goes here. meeting title goes here.</h2>
-                <span className="remain">7명 남음</span>
-                <time>2017.12.12</time>
+                <h2 className="meeting-title">Design</h2>
+                <span className="meeting-content">Freelance Design Tricks How To Get Away With Murder in The Workplace</span>
               </div>
-              <div className="progress" style={{width: 60 + '%'}}></div>
+              <div className="meeting-item-footer">
+                <span className="location">7596 Launine Parkway</span>
+                <time>15 Mar 2018</time>
+              </div>
             </Link>
             <Link to="#" className="meeting-item">
               <div className="meeting-image"></div>
               <div className="meeting-item-body">
-                <h2 className="meeting-title">meeting title goes here. meeting title goes here.</h2>
-                <span className="remain">7명 남음</span>
-                <time>2017.12.12</time>
+                <h2 className="meeting-title">Design</h2>
+                <span className="meeting-content">Freelance Design Tricks How To Get Away With Murder in The Workplace</span>
               </div>
-              <div className="progress" style={{width: 60 + '%'}}></div>
+              <div className="meeting-item-footer">
+                <span className="location">7596 Launine Parkway</span>
+                <time>15 Mar 2018</time>
+              </div>
             </Link>
             <Link to="#" className="meeting-item">
               <div className="meeting-image"></div>
               <div className="meeting-item-body">
-                <h2 className="meeting-title">meeting title goes here. meeting title goes here.</h2>
-                <span className="remain">7명 남음</span>
-                <time>2017.12.12</time>
+                <h2 className="meeting-title">Design</h2>
+                <span className="meeting-content">Freelance Design Tricks How To Get Away With Murder in The Workplace</span>
               </div>
-              <div className="progress" style={{width: 60 + '%'}}></div>
+              <div className="meeting-item-footer">
+                <span className="location">7596 Launine Parkway</span>
+                <time>15 Mar 2018</time>
+              </div>
             </Link>
           </div>
         </div>
